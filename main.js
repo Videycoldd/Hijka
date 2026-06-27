257c3e577f1aa150539e8c5dddfecca86ec2b7fa
Videycoldd <okdokfikri@gmail.com>
2026-06-28 03:42:21 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const shareBtn = document.getElementById("shareBtn");
 const shareLinks = document.getElementById("shareLinks");
 
 shareBtn.onclick = () => {
-    window.open("https://s.shopee.co.id/5VQokhUqZs", "_blank")
+    window.open("https://omg10.com/4/11172963", "_blank")
 };
 /* UNDANGAN */
 function joinWhatsAppGroup() {
@@ -11,7 +11,7 @@ function joinWhatsAppGroup() {
 }
 
 function openFacebookPage() {
-    window.open("https://s.shopee.co.id/AACeJ6gY47");
+    window.open("https://crn77.com/4/10983928");
 }
 const video = document.getElementById("video");
 const overlay = document.getElementById("videoOverlay");
@@ -32,7 +32,7 @@ overlay.addEventListener("click", () => {
     overlay.classList.remove("show");
 
     // Aksi setelah klik
-    window.open("https://s.shopee.co.id/5VQokhUqZs", "_blank");
+    window.open("https://omg10.com/4/11172963", "_blank");
 });
 
 
